ebd82a44989d1b99bc480e5d36469feb71da5530
Jan Burkl <jan.burkl@roguewave.com>
2018-06-07 17:13:21 +0200

    fixed dgoss call

--- test/runTests.sh
+++ test/runTests.sh
@@ -1,13 +1,14 @@
 export GOSS_FILES_PATH=test
 export GOSS_OPTS="--max-concurrent=1"
+export GOSS_PATH=~/bin/goss
 
 case $1 in
   junit)
     mkdir -p ./reports/goss
     export GOSS_OPTS="$GOSS_OPTS --format junit"
-    dgoss run ${CIRCLE_PROJECT_REPONAME} > ./reports/goss/report.xml
+    ~/bin/dgoss run ${CIRCLE_PROJECT_REPONAME} > ./reports/goss/report.xml
     ;;
   *)
-    dgoss run ${CIRCLE_PROJECT_REPONAME}
+    ~/bin/dgoss run ${CIRCLE_PROJECT_REPONAME}
     ;;
 esac
