1a5c3a131b646a44f8838af9462c8519d8b37803
Jan Burkl <jan.burkl@roguewave.com>
2018-06-08 14:45:34 +0200

    removed false fix

--- test/runTests.sh
+++ test/runTests.sh
@@ -9,9 +9,9 @@ case $1 in
   junit)
     mkdir -p ~/reports/goss
     export GOSS_OPTS="$GOSS_OPTS --format junit"
-    test/dgoss run --entrypoint=/bin/sh ${CIRCLE_PROJECT_REPONAME} sleep 30 > ~/reports/goss/report.xml
+    test/dgoss run ${CIRCLE_PROJECT_REPONAME} > ~/reports/goss/report.xml
     ;;
   *)
-    test/dgoss run --entrypoint=/bin/sh ${CIRCLE_PROJECT_REPONAME} sleep 30
+    test/dgoss run ${CIRCLE_PROJECT_REPONAME}
     ;;
 esac
